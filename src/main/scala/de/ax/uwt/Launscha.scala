fc441f28f59cb0575f541f9d37a9af6f651c495b
nyxos <axaluss@gmail.com>
2017-07-12 21:18:56 +0200

    Launscha

---
+++ src/main/scala/de/ax/uwt/Launscha.scala
@@ -0,0 +1,18 @@
+package de.ax.uwt
+
+import cats.instances.future
+
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+/**
+  * Created by nyxos on 11.07.17.
+  */
+object Launscha extends App {
+
+
+  val runner=Future{
+    new PiRun().run
+  }
+  WebServer.main(Array.empty)
+
+}
